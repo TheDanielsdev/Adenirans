3dce594f7219bbcbd17b5927a1ee23fd68862f83
TheDaniels <thedaniels@gmail.com>
2020-11-24 20:26:47 +0100

    few more background and images

---
+++ mine.js
@@ -0,0 +1,9 @@
+const btn_one = document.querySelector('.btn_one').addEventListener('click',function(ev){
+    alert('Thanks for submitting')
+    console.log(ev.type)
+})
+
+
+/*inpt = document.querySelector('.inpt').addEventListener('input',function(){
+    inpt.textContext = this.value;
+})*/
